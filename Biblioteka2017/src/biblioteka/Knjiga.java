95308aeae32c86dba0f1f8ea423292f346bcb562
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-14 15:18:19 +0100

    Uvedena klasa Autor, refaktorisana lista autora u klasi Knjiga

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -1,12 +1,14 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 
 	private String naslov;
 	private long isbn;
 	private String izdavac;
 	private int izdanje;
-	private String autori;
+	private LinkedList<Autor> autori;
 
 	public String getNaslov() {
 		return naslov;
@@ -40,11 +42,11 @@ public class Knjiga {
 		this.izdanje = izdanje;
 	}
 
-	public String getAutori() {
+	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
 
-	public void setAutori(String autori) {
+	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
 
