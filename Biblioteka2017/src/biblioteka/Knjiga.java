008a31f6b9e60ba2ccea72a6ac209218c116c857
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-14 15:35:55 +0100

    Dodat toString, hashCode, equals u klasi Knjiga i Autor

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -50,4 +50,32 @@ public class Knjiga {
 		this.autori = autori;
 	}
 
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + autori + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn != other.isbn)
+			return false;
+		return true;
+	}
+
 }
