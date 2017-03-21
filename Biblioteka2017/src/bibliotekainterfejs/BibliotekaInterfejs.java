4683ba49eee4d1c3d80b71f63f9f23e29f0f2001
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-21 14:51:28 +0100

    Uvedena promena u klasu Biblioteka

--- Biblioteka2017/src/bibliotekainterfejs/BibliotekaInterfejs.java
+++ Biblioteka2017/src/bibliotekainterfejs/BibliotekaInterfejs.java
@@ -9,7 +9,7 @@ public interface BibliotekaInterfejs {
 	
 	public void dodajKnjigu(Knjiga k);
 	public void obrisiKnjigu(Knjiga k);
-	public LinkedList<Knjiga> vratiSveKnjige(Knjiga k);
+	public LinkedList<Knjiga> vratiSveKnjige();
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor a, long isbn, String naslov, String izdavac);
 
 }
